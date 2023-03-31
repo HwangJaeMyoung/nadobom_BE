c607df6f41cfad87683e30915dadc5ba43363263
HwangJaeMyoung <hjmhjm0329@gmail.com>
2023-03-31 20:16:58 +0900

    mainserver 수정

--- main/mainServer/mainServer/settings.py
+++ main/mainServer/mainServer/settings.py
@@ -32,7 +32,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
